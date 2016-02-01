330e63a12caa69daced1f17486f2a18445b07355
Vanecie Delva <vanecie.delva@gmail.com>
2016-01-31 16:59:23 -0800

    Solutions to something I don't want to delete. Fuck you Alistair! HA!

--- solutions1-new.py
+++ solutions1-new.py
@@ -3,44 +3,79 @@
 # 1) Write a function which takes two strings strA and strB and returns them joined with a space
 #    "hello", "world" -> "hello world"
 def solution1(strA, strB):
+	return strA + " " + strB
+	
 
 # 2) Write a function which adds 2 integers A and B and returns the result
 #    a, b -> a + b
 def solution2(A, B):
+	return A + B
 
 # 3) Write a function which returns the integers 0 -> 9 (inclusive) as an array
 #    -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 def solution3():
+	return range (0,10)
+	
 
 # 4) Write a function which returns the even integers between 0 -> 9 as an array
 #    -> [0, 2, 4, 6, 8]
 def solution4():
+	return range (0,10,2)
 
 # 5) Write a function which inverts the capitalization of the string A and returns the result
 #    (You cannot use swapcase())
 #    "ThiS IS a STrinG" -> "tHIs is A stRINg"
+
 def solution5(A):
+	new_shit = ""
+	for char in A:
+		if char.islower():
+			new_shit = new_shit + char.upper()
+			#print(char.upper())
+		else:
+			new_shit = new_shit + char.lower()
+			#print(char.lower())
+	return new_shit
 
 # 6) Write a function which returns a string containing only words from another string over 4 letters long
 #    The returned string should have words delimited by spaces. Don't leave spaces at the beginning or end of the returned string
 #    "this is a really complicated string" -> "really complicated string"
-
 def solution6(s):
+	proud = ""
+	for fabz in s.split(" "): 
+		if len(fabz) >= 5: 
+			proud = proud + " " + fabz
+	return proud.strip()
+		#print (len(fabz))
 
 # 7) Write a function which returns an array containing integers in the Fibonacci sequence up to 377
 #    https://en.wikipedia.org/wiki/Fibonacci#Fibonacci_sequence 
 #	1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377
 def solution7():
+	array = [1,1]
+	while array[-1] < 377:
+		array.append(array[-1] + array[-2])
+	return array
 
 # 8) Write a function which returns an array containing the first 10 prime numbers
 #    (Zero and one are not prime numbers)
 # 2,3,5,7,11,13,17,19,23,29
 def solution8():
+	primes = [] 
+	x = 2
+	while len(primes) <10: 
+		for minaj in range(2,x):
+			if x % minaj == 0:
+				break 
+		
+		x = x+1
+		print (primes)
 
 # 9) Write a function which takes a string, which is an arithmetic expression containing only floats and one of +,-,/,*, and returns the result of the expression as a string
 #    Normal rules for operator precedence do not apply, just use the operators in the sequence you read them
 #    "50.0+60.0/10.0*20.0" -> "220.0"
 def solution9(expr):
+	return
 
 # IGNORE EVERYTHING BELOW THIS LINE
 
